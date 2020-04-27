538f606432b3739e07d5c1f4c51f0308986e66a7
David Vossel <davidvossel@gmail.com>
2020-04-27 15:04:29 -0400

    Update unit tests to use dynamic supported api versions instead of hardcorded v1alpha3
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/virt-operator/creation/components/apiservices_test.go
+++ pkg/virt-operator/creation/components/apiservices_test.go
@@ -3,13 +3,15 @@ package components
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	v1 "kubevirt.io/client-go/api/v1"
 )
 
 var _ = Describe("APIServices", func() {
 
 	It("should load one APIService with the correct namespace", func() {
 		services := NewVirtAPIAPIServices("mynamespace")
-		Expect(services).To(HaveLen(1))
+		Expect(services).To(HaveLen(len(v1.SubresourceGroupVersions)))
 		Expect(services[0].Spec.Service.Namespace).To(Equal("mynamespace"))
 	})
 })
